d96643fbbf5eb3b6e7a9ca2e5bed1d26700b8981
Ishtiak Ahmed Moyen <116382978+moyen-ahmed@users.noreply.github.com>
2024-11-04 00:14:17 +0600

    Variable 1st lecture

---
+++ print.dart
@@ -0,0 +1,16 @@
+void main(){
+  // print("abir");
+  // print(3+2);
+  String somevalue ='Moyen';
+  dynamic valu='1211';
+  print(somevalue);
+  print(valu.runtimeType);
+ final valu1=DateTime.now();
+ const valu2='1000';
+ print(valu1);
+ print(valu2);
+
+ //Ternary
+ String value=somevalue.startsWith('M')? 'Hi' :'bye';
+ print(value);
+}
